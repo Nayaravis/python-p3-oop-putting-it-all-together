da50d9689c9e092b5477ff2de43e03c410ffd51a
Navis Ochieng <navis.ochieng@student.moringaschool.com>
2025-08-09 18:37:21 +0300

    lab complete

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, shoe_size):
+        if not isinstance(shoe_size, int):
+            print("size must be an integer")
+        else:
+            self._size = shoe_size
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
